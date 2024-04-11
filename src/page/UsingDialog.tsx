e8bd52bb47b410212d5894f7af7f45f5f73eff33
kheeyaa <kheeyaa@icloud.com>
2024-04-11 23:05:23 +0900

    docs: Update Dialog Example

--- src/page/UsingDialog.tsx
+++ src/page/UsingDialog.tsx
@@ -1,4 +1,4 @@
-import { Dialog, useDialog } from "../../lib/components/Dialog/Dialog"
+import {Dialog, useDialog} from '../../lib/components/Dialog/Dialog'
 
 export default function UsingDialog() {
   const dialog = useDialog()
@@ -13,13 +13,14 @@ export default function UsingDialog() {
         className="dialog"
         context={dialog}
       >
-
         <Dialog.Title>h1 - default</Dialog.Title>
         <Dialog.Title as="h2">h2</Dialog.Title>
         <Dialog.Title as="h3">h3</Dialog.Title>
 
         <Dialog.Contents>contents1</Dialog.Contents>
-        <Dialog.Contents onClick={() => alert('hey!')}>Clickable contents</Dialog.Contents>
+        <Dialog.Contents onClick={() => alert('hey!')}>
+          Clickable contents
+        </Dialog.Contents>
 
         <div style={{border: '1px solid red'}}>
           <Dialog.Contents>This Content is in the "div"</Dialog.Contents>
@@ -27,7 +28,7 @@ export default function UsingDialog() {
 
         <Dialog.Button
           style={{border: '1px sold gray', background: 'white'}}
-          closed
+          onClick={() => console.log('hello!')}
         >
           close button
         </Dialog.Button>
@@ -43,32 +44,31 @@ export default function UsingDialog() {
         >
           close button3
         </Dialog.Button>
-        <Dialog.Button
-          as="a"
-          onClick={dialog.close}
-        >
+        <Dialog.Button as="a" onClick={dialog.close}>
           close a link
         </Dialog.Button>
-        <Dialog.Button
-          as="div"
-          onClick={dialog.close}
-        >
+        <Dialog.Button as="div" onClick={dialog.close}>
           close div
         </Dialog.Button>
       </Dialog>
       <button onClick={dialog2.open}>trigger2</button>
       <Dialog
-        style={{border: '1px solid black', background: 'skyblue', padding: '10px'}}
+        style={{
+          border: '1px solid black',
+          background: 'skyblue',
+          padding: '10px',
+        }}
         className="dialog"
         context={dialog2}
       >
-
         <Dialog.Title>h1 - default</Dialog.Title>
         <Dialog.Title as="h2">h2</Dialog.Title>
         <Dialog.Title as="h3">h3</Dialog.Title>
 
         <Dialog.Contents>contents1</Dialog.Contents>
-        <Dialog.Contents onClick={() => alert('hey!')}>Clickable contents</Dialog.Contents>
+        <Dialog.Contents onClick={() => alert('hey!')}>
+          Clickable contents
+        </Dialog.Contents>
 
         <div style={{border: '1px solid red'}}>
           <Dialog.Contents>This Content is in the "div"</Dialog.Contents>
@@ -80,16 +80,10 @@ export default function UsingDialog() {
         >
           close button
         </Dialog.Button>
-        <Dialog.Button
-          as="a"
-          onClick={dialog2.close}
-        >
+        <Dialog.Button as="a" onClick={dialog2.close}>
           close a link
         </Dialog.Button>
-        <Dialog.Button
-          as="div"
-          onClick={dialog2.close}
-        >
+        <Dialog.Button as="div" onClick={dialog2.close}>
           close div
         </Dialog.Button>
       </Dialog>
